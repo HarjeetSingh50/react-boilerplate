7689771ce36f8684288638b392c18b1bda03b4c5
harjeetteamlance <harjeet@teamlance.io>
2020-04-02 14:06:29 +0530

    Working on login form

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import { AvForm, AvField, AvGroup } from "availity-reactstrap-validation";
 
 function Login() {
   return (
     <div className="content-wrapper">
-      <div className="content-header">
-        <div className="container">
-          <div className="row mb-2">
-            <div className="col-sm-6">
-              <h1 className="m-0 text-dark">Start Login Here</h1>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="content">
         <div className="container">
           <div className="row">
-            <div className="col-lg-12">
-              <h3>Login Form will be there</h3>
+            <div className="col-lg-6 mt-5">
+              <div class="card card-info">
+                <div class="card-header">
+                  <h3 class="card-title">Login</h3>
+                </div>
+                <AvForm class="form-horizontal">
+                  <div class="card-body">
+                    <AvGroup className="row">
+                      <label for="email" class="col-sm-2 col-form-label">Email</label>
+                      <div class="col-sm-10">
+                        <AvField
+                          name="email"
+                          type="text"
+                          placeholder="Enter email"
+                          required errorMessage="Please enter email."
+                        />
+                      </div>
+                    </AvGroup>
+                    <AvGroup className="row">
+                      <label for="password" class="col-sm-2 col-form-label">Password</label>
+                      <div class="col-sm-10">
+                        <AvField
+                          name="password"
+                          type="password"
+                          placeholder="Enter password."
+                          required errorMessage="Please enter password."
+                        />
+                      </div>
+                    </AvGroup>
+                  </div>
+                  <div class="card-footer">
+                    <button type="submit" class="btn btn-info">Sign in</button>
+                  </div>
+                </AvForm>
+              </div>
             </div>
           </div>
         </div>
